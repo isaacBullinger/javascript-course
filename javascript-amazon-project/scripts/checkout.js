97c6e9f64476f41b5cb0244a03b755f22f213cc1
isaacBullinger <isaac.k.bullinger@gmail.com>
2024-05-08 10:51:40 -0500

    Make delivery options interactive.

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart, calculateCartQuantity, updateQuantity } from "../data/cart.js";
+import { cart, removeFromCart, calculateCartQuantity, updateQuantity, updateDeliveryOption } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
@@ -91,25 +91,24 @@ cart.forEach((cartItem) => {
 function deliveryOptionsHTML(matchingProduct, cartItem) {
     let html = '';
 
-    deliveryOptions.forEach((deliveryOptions) => {
+    deliveryOptions.forEach((deliveryOption) => {
         const today = dayjs();
         const deliveryDate = today.add(
-            deliveryOptions.deliveryDays,
+            deliveryOption.deliveryDays,
             'days'
         );
         const dateString = deliveryDate.format(
             'dddd, MMMM D'
         );
 
-        const priceString = deliveryOptions
-        === 0
+        const priceString = deliveryOption.priceCents === 0
             ? 'FREE'
-            : `$${formatCurrency(deliveryOptions.priceCents)} -`;
+            : `$${formatCurrency(deliveryOption.priceCents)} -`;
         
-        const isChecked = deliveryOptions.id === cartItem
+        const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
 
         html += `
-            <div class="delivery-option">
+            <div class="delivery-option js-delivery-option" data-product-id="${matchingProduct.id}" data-delivery-option-id="${deliveryOption.id}">
                 <input type="radio"
                     ${isChecked ? 'checked' : ''}
                     class="delivery-option-input"
@@ -222,3 +221,11 @@ function validate(newQuantity) {
         return;
     }
 }
+
+document.querySelectorAll('.js-delivery-option')
+    .forEach((element) => {
+        element.addEventListener('click', () => {
+            const {productId, deliveryOptionId} = element.dataset;
+            updateDeliveryOption(productId, deliveryOptionId)
+        })
+    });

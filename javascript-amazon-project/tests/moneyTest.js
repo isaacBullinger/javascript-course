91540896f60cda1b685ff8f293476fdb9efd14c1
isaacBullinger <isaac.k.bullinger@gmail.com>
2024-05-11 15:12:10 -0500

    Remove default tests and add money tests.

--- javascript-amazon-project/tests/moneyTest.js
+++ javascript-amazon-project/tests/moneyTest.js
@@ -2,7 +2,7 @@ import { formatCurrency } from '../scripts/utils/money.js';
 
 console.log('test suite: formatCurrency');
 
-console.log('converts cents to dollars:');
+console.log('converts cents to dollars');
 
 if (formatCurrency(2095) === '20.95') {
    console.log('passed');
@@ -10,7 +10,7 @@ if (formatCurrency(2095) === '20.95') {
    console.log('failed');
 }
 
-console.log('works with 0:')
+console.log('works with 0')
 
 if (formatCurrency(0) === '0.00') {
    console.log('passed');
@@ -18,7 +18,7 @@ if (formatCurrency(0) === '0.00') {
    console.log('failed');
 }
 
-console.log('rounds up to the nearest cent:');
+console.log('rounds up to the nearest cent');
 
 if (formatCurrency(2000.5) === '20.01') {
    console.log('passed');

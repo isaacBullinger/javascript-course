98a52a64de4197736747b8be09d2566aabc8940f
isaacBullinger <isaac.k.bullinger@gmail.com>
2024-05-12 15:54:27 -0500

    Tests for rounding down and negative numbers for addToCart.

--- javascript-amazon-project/tests/tests-simple/moneyTest.js
+++ javascript-amazon-project/tests/tests-simple/moneyTest.js
@@ -25,3 +25,19 @@ if (formatCurrency(2000.5) === '20.01') {
 } else {
    console.log('failed');
 }
+
+console.log('rounds down to the nearest cent');
+
+if (formatCurrency(2000.4) === '20.00') {
+      console.log('passed');
+   } else {
+      console.log('failed');
+}
+
+console.log('negative number');
+
+if (formatCurrency(-2095) === '-20.95') {
+      console.log('passed');
+   } else {
+      console.log('failed');
+}

c0ce1be906c7c411e6178d481833326e4ffc59b5
isaacBullinger <isaac.k.bullinger@gmail.com>
2024-05-20 12:53:29 -0500

    Added RaceCar class that inherited from Car.

--- javascript-amazon-project/data/car.js
+++ javascript-amazon-project/data/car.js
@@ -1,28 +1,65 @@
 class Car {
-    brand;
-    model;
-    speed;
+    #brand;
+    #model;
+    #speed;
+    isTrunkOpen;
 
     constructor (carDetails) {
-        this.brand = carDetails.brand;
-        this.model = carDetails.model;
+        this.#brand = carDetails.brand;
+        this.#model = carDetails.model;
         this.speed = 0;
+        this.isTrunkOpen = false;
     }
 
     displayInfo() {
-        console.log(`${this.brand} ${this.model}, Speed: ${this.speed} km/h`);
+        const trunkStatus = this.isTrunkOpen ? 'open' : 'closed';
+
+        console.log(`${this.#brand} ${this.#model}, Speed: ${this.speed} km/h Trunk: ${trunkStatus}`);
     }
 
-    go(rate) {
-        if (this.speed < 200) {
-            this.speed += rate;
+    go() {
+        if (this.speed < 200 && !this.isTrunkOpen) {
+            this.speed += 5;
         }
     }
 
-    brake(rate) {
+    brake() {
         if (this.speed > 0) {
-            this.speed -= rate;
+            this.speed -= 5;
         }
+    }
+
+    openTrunk() {
+        if (this.speed === 0) {
+            this.isTrunkOpen = true;
+        }
+    }
+
+    closeTrunk() {
+        this.isTrunkOpen = false;
+    }
+}
+
+class RaceCar extends Car {
+    acceleration;
+
+    constructor(carDetails) {
+        super(carDetails);
+        this.acceleration = carDetails.acceleration;
+    }
+
+    go() {
+        if (this.speed < 300) {
+            this.speed += this.acceleration;
+        }
+    }
+
+    openTrunk() {
+        console.log('No trunk')
+    }
+
+    closeTrunk() {
+        console.log('No trunk')
     }
 }
 
@@ -36,18 +73,33 @@ const car2 = new Car({
     model: 'Model 3'
 });
 
+const raceCar = new RaceCar({
+    brand: 'McLaren',
+    model: 'F1',
+    acceleration: 20
+});
+
 console.log(car1);
 console.log(car2);
 
-car1.go(100);
-car1.go(95);
-car1.go(5);
-car1.go(5);
-car1.go(5);
+car1.openTrunk();
+car1.go();
+car1.go();
+car1.go();
+car1.go();
+car1.go();
 
-car2.go(45);
-car2.brake(5);
-car2.brake(5);
+car2.openTrunk();
+car2.go();
+car2.closeTrunk();
+car2.go();
+car2.go();
+
+raceCar.openTrunk();
+raceCar.go();
+raceCar.go();
+raceCar.brake();
 
 car1.displayInfo();
 car2.displayInfo();
+raceCar.displayInfo();

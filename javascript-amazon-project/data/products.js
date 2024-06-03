e66b297e5391bddcb6a71b6a17cbc1fd32fb5431
isaacBullinger <isaac.k.bullinger@gmail.com>
2024-06-03 12:50:46 -0500

    Learn error handling.

--- javascript-amazon-project/data/products.js
+++ javascript-amazon-project/data/products.js
@@ -127,10 +127,13 @@ export function loadProductsFetch() {
     });
 
     console.log('load products');
+  }).catch((error) => {
+    console.log('Unexpected error. Please try again later.');
   });
 
   return promise;
 }
+
 /*
 loadProductsFetch().then(() => {
   console.log('next step')
@@ -153,6 +156,10 @@ export function loadProducts(fun) {
     console.log('load products');
 
     fun();
+  });
+
+  xhr.addEventListener('error', (error) => {
+    console.log('Unexpected error. Please try again later.');
   });
 
   xhr.open('GET', 'https://supersimplebackend.dev/products');
--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -8,14 +8,22 @@ import { loadCart } from "../data/cart.js";
 import "../data/car.js";
 
 async function loadPage() {
+    try {
+        // throw 'error1';
 
-    await loadProductsFetch();
+        await loadProductsFetch();
 
-    await new Promise((resolve) => {
-        loadCart(() => {
-            resolve();
+        const value = await new Promise((resolve, reject) => {
+            // throw 'error2';
+            loadCart(() => {
+                // reject('error3');
+                resolve('value3');
+            });
         });
-    })
+    
+    } catch (error) {
+        console.log('Unexpected error. Please try again later.');
+    }
 
     renderCheckoutHeader();
     renderOrderSummary();
